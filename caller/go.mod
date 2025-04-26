56f52e407317c17ea332b9046a17e7c5311a63ae
remi <remi.espie@etu.umontpellier.fr>
2025-04-26 11:52:05 +0200

    wip: tts

--- caller/go.mod
+++ caller/go.mod
@@ -11,6 +11,9 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.3 // indirect
+	github.com/hajimehoshi/oto/v2 v2.2.0 // indirect
+	github.com/hegedustibor/htgo-tts v0.0.0-20240912200108-467b3e535435 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/sagikazarmark/locafero v0.9.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- caller/main.go
+++ caller/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	htgotts "github.com/hegedustibor/htgo-tts"
+	"github.com/hegedustibor/htgo-tts/voices"
 	"github.com/spf13/viper"
 	"log"
 	"strings"
@@ -11,11 +13,17 @@ import (
 func main() {
 	event := loadConfig("EVENT")
 	ollamaHost := loadConfig("OLLAMA_HOST")
-	piperHost := loadConfig("PIPER_HOST")
 	whisperHost := loadConfig("WHISPER_HOST")
 
 	log.Println("Using event:", event)
+
 	ollama := callPrompt(ollamaHost, event)
+	// Local TTS
+	speech := htgotts.Speech{Folder: "audio", Language: voices.French}
+	err := speech.Speak(ollama)
+	if err != nil {
+		log.Fatal("Error transforming text to speech:", err)
+	}
 }
 
 func loadConfig(configString string) string {
