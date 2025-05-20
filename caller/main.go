b23dbb11414619ceb97c616e801d21b30039bd7a
Kuruyia <github@kuruyia.net>
2025-05-20 15:21:50 +0200

    feat: get the event from the kubeapi
    Signed-off-by: Kuruyia <github@kuruyia.net>

--- caller/main.go
+++ caller/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"github.com/google/uuid"
+	"log"
+	"strings"
+
 	htgotts "github.com/hegedustibor/htgo-tts"
 	"github.com/hegedustibor/htgo-tts/handlers"
 	"github.com/hegedustibor/htgo-tts/voices"
 	"github.com/spf13/viper"
-	"log"
-	"strings"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	event := loadConfig("EVENT")
+	eventName := loadConfig("EVENT_NAME")
+	eventNamespace := loadConfig("EVENT_NAMESPACE")
 	ollamaHost := loadConfig("OLLAMA_HOST")
 	whisperHost := loadConfig("WHISPER_HOST")
 
-	log.Println("Using event:", event)
+	log.Println("Using event", eventName, "in namespace", eventNamespace)
 
-	oc, ollama := initPrompt(ollamaHost, event)
+	clientSet := createK8sClient()
+	event := getEvent(clientSet, eventName, eventNamespace)
+
+	oc, ollama := initPrompt(ollamaHost, event.Message)
 	log.Println("Ollama response:", ollama)
+
 	// Local TTS
 	speech := htgotts.Speech{Folder: "audio", Language: voices.English, Handler: &handlers.Native{}}
 	filepath, err := speech.CreateSpeechFile(ollama, uuid.New().String())
@@ -56,4 +67,27 @@ func loadConfig(configString string) string {
 	viper.SetDefault(configString, *config)
 
 	return viper.GetString(configString)
+}
+
+func createK8sClient() *kubernetes.Clientset {
+	clientConfig, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(clientcmd.NewDefaultClientConfigLoadingRules(), &clientcmd.ConfigOverrides{}).ClientConfig()
+	if err != nil {
+		log.Fatal("Error creating the Kubernetes client config:", err)
+	}
+
+	clientSet, err := kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		log.Fatal("Error creating the Kubernetes client set:", err)
+	}
+
+	return clientSet
+}
+
+func getEvent(clientSet *kubernetes.Clientset, eventName string, eventNamespace string) *corev1.Event {
+	event, err := clientSet.CoreV1().Events(eventNamespace).Get(context.TODO(), eventName, metav1.GetOptions{})
+	if err != nil {
+		log.Fatal("Error getting the event:", err)
+	}
+
+	return event
 }
